e6a6cd771d975a548b4379b6c4a7a5e12c1dc275
Ibrahim Abdullahi <ibrahymabdullahi2018@gmail.com>
2025-10-24 01:12:04 +0100

    adding

--- src/adminDashboard/AdminDashboard.jsx
+++ src/adminDashboard/AdminDashboard.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Topbar from "./Topbar";
 import StatsCards from "./StatsCards";
 import AdminHouses from "../pages/AdminHouses";
 import ApprovedHouses from "./Approvedhouses";
@@ -24,9 +23,6 @@ export default function AdminDashboard() {
 
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col">
-      {/* Topbar */}
-      <Topbar activeTab={activeTab} setActiveTab={setActiveTab} />
-
       {/* Main Content */}
       <main className="p-4 sm:p-6 space-y-6 flex-1">
         {/* ✅ Pass both props so StatsCards knows which one is active */}
